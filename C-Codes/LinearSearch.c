f600bee3239c10533a91541cc545131e1ddbee3c
Pratik Pandey <pratiik.pandey@gmail.com>
2021-01-29 14:50:44 +0530

    first commit

--- C-Codes/LinearSearch.c
+++ C-Codes/LinearSearch.c
@@ -5,53 +5,30 @@
 //  Created by PRATIK PANDEY on 29/01/21.
 //Linear Search
 
-//#include <stdio.h>
-//int LinearSearch(int arr[],int size,int element){
-//    for(int i=0;i<size;i++){
-//        if(arr[i]==element){
-//            return i;
-//        }
-//    }
-//    return -1;
-//}
-//
-//
-//int main() {
-//    int arr[]={1,5,6,7,3,44,82,23,53,90};
-//    int size=sizeof(arr)/sizeof(int);
-//    int element=82;
-//    int searchindex=LinearSearch(arr, size, element);
-//    printf("The Element %d was Found At %d index\n",element,searchindex);
-//    return 0;
-//}
-#include <stdio.h>
-int BinarySearch(int arr[],int size,int element){
-    int low,mid,high;
-    low=0;
-    high=size-1;
-    mid=(low+high)/2;
-    while(low<=high){
-        if(arr[mid]==element){
-            return mid;
-        }
-        if(element>arr[mid]){
-            low=mid+1;
 
-        }
-        else{
-            high=mid-1;
+#include<stdio.h>
+
+int linearSearch(int arr[], int size, int element){
+    for (int i = 0; i < size; i++)
+    {
+        if(arr[i]==element){
+            return i;
         }
     }
     return -1;
 }
 
+
 int main(){
-   
-    int arr[]={1,3,5,7,9,22,34,75};
-    int size=sizeof(arr)/sizeof(int);
-    int element=5;
-    int searchedindex=BinarySearch(arr,size,element);
-    printf("The Element %d is found at %d\n",element,searchedindex);
+    // Unsorted array for linear search
+     int arr[] = {1,3,5,56,4,3,23,5,4,54634,56,34};
+     int size = sizeof(arr)/sizeof(int);
+
+
+    int size = sizeof(arr)/sizeof(int);
+    int element = 5
+    int searchIndex = linearSearch(arr, size, element);
+    printf("The element %d was found at index %d \n", element, searchIndex);
     return 0;
 }
 
